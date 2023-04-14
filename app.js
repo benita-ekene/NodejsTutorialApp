ea39af078b8befc644737bc1153614ed4cdf4354
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-04-14 06:27:55 +0100

    updated

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const user = require('./UserObject');
 
 const app = express();
 
-//create route handlers/endpoints
+//create route handlers/endpoints to get individual user
 app.get('/user/:id', (req, res) => {
  res.json(user.filter(user1 => user1.id === parseInt(req.params.id)))
 })
 
 app.post('/user/:name', (req, res) => {
-  res.json(user.filter(user2 => user2.name === req.params.name))
+  res.json(user.filter(user1 => user1.name === req.params.name))
+})
+
+app.delete('/user/:age', (req, res) => {
+  res.json(user.filter(user1 => user1.age === req.params.age))
 })
 
 app.put('/user/:sex', (req, res) => {
-  res.json(user.filter(user2 => user2.sex === req.params.sex))
+  res.json(user.filter(user1 => user1.sex === req.params.sex))
 })
 
 app.listen(5000)
