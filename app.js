034a78ee9ce2078697d406ea18dea684bcbba9c1
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-04-14 05:51:16 +0100

    initial commit

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const app = express();
 
 //create route handlers/endpoints
 app.get('/user/:id', (req, res) => {
-  res.json(user.filter(user1 => user1.id === parseInt(req.params.id)))
+ res.json(user.filter(user1 => user1.id === parseInt(req.params.id)))
 })
+
+app.post('/user/:name', (req, res) => {
+  res.json(user.filter(user2 => user2.name === req.params.name))
+})
+
+app.put('/user/:sex', (req, res) => {
+  res.json(user.filter(user2 => user2.sex === req.params.sex))
+})
+
 app.listen(5000)
  
